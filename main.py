85ee564e81d8ee31b38d70fb1ed6e7a31a68c7ce
D365-admin <d365-admin@hiqelectronics.com>
2026-05-21 00:04:54 +0530

    commit

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
